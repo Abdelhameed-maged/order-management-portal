fdb3584640154766bfeab0d2367f71fa3059894c
abdelhameed-maged <abdelhmedahmed94@gmail.com>
2024-10-22 11:15:21 +0300

    upgrade to latest version

--- src/app/api/api.module.ts
+++ src/app/api/api.module.ts
@@ -2,19 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OrdersServiceService } from './services/orders-service.service';
 import { ProductsServiceService } from './services/products-service.service';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 
 
-@NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    HttpClientModule
-  ],
-  providers: [
-    OrdersServiceService,
-    ProductsServiceService
-  ]
-})
+@NgModule({ declarations: [], imports: [CommonModule], providers: [
+        OrdersServiceService,
+        ProductsServiceService,
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class ApiModule { }
